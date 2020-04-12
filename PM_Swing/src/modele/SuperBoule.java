e052afd438d6fc6f45cd6ef1eea26c9244b4ebef
Julien Giraud <giraud740@gmail.com>
2020-04-12 18:17:32 +0200

    Avec le fichier superboule c'est mieux

---
+++ PM_Swing/src/modele/SuperBoule.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modele;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author julien
+ */
+public class SuperBoule extends Boule {
+
+    public SuperBoule(Jeu _jeu, Point p) {
+        super(_jeu, p);
+        score = 50;
+    }
+
+}
