3800e69167b4edc04f361f8e2642a81959c07969
Julien Giraud <giraud740@gmail.com>
2020-04-21 20:12:14 +0200

    variable Main

--- PM_Swing/src/Main.java
+++ PM_Swing/src/Main.java
@@ -8,8 +8,7 @@ public class Main {
         
         // Le debug à true permet de placer juste quelques boules pour tester
         // toutes les fonctionnalités du jeu en quelques secondes
-        
-        new VueControleurPacMan(debug); // C'est la vue qui fait tout
+        VueControleurPacMan vue = new VueControleurPacMan(debug); // C'est la vue qui fait tout
 
     }
 }
